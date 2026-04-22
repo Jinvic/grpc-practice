28513883afa090433860bca52541aa7c629a320c
Jinvic <2528976435@qq.com>
2026-04-22 11:16:42 +0800

    feat: add database configuration to application
    - Introduced database configuration settings in config.yml for path, max open connections, max idle connections, and connection max lifetime.
    - Updated Config struct in config.go to include Database settings.
    - Implemented default values for database configuration in viper.go to ensure proper initialization.

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,8 +1,11 @@
 package config
 
+import "time"
+
 type Config struct {
 	Environment string   `mapstructure:"environment"` // dev, staging, prod
 	Services    Services `mapstructure:"services"`
+	Database    Database `mapstructure:"database"`
 }
 
 type Services struct {
@@ -12,4 +15,11 @@ type Services struct {
 type BookService struct {
 	Host string `mapstructure:"host"`
 	Port int    `mapstructure:"port"`
+}
+
+type Database struct {
+	Path            string        `mapstructure:"path"`              // 数据库文件路径
+	MaxOpenConns    int           `mapstructure:"max_open_conns"`    // 最大连接数
+	MaxIdleConns    int           `mapstructure:"max_idle_conns"`    // 最大空闲连接数
+	ConnMaxLifetime time.Duration `mapstructure:"conn_max_lifetime"` // 连接最大生命周期
 }
--- internal/pkg/config/viper.go
+++ internal/pkg/config/viper.go
@@ -12,6 +12,9 @@ func GetConfig(i do.Injector) (*Config, error) {
 	viper.SetConfigType("yml")
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("./config")
+
+	setDefaultConfig(viper.GetViper())
+
 	err := viper.ReadInConfig()
 	if err != nil {
 		return nil, fmt.Errorf("failed to read config: %w", err)
@@ -22,4 +25,14 @@ func GetConfig(i do.Injector) (*Config, error) {
 		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
 	}
 	return &cfg, nil
+}
+
+func setDefaultConfig(v *viper.Viper) {
+	v.SetDefault("database.path", "./data/bookstore.db")
+	v.SetDefault("database.max_open_conns", 1)
+	v.SetDefault("database.max_idle_conns", 1)
+	v.SetDefault("database.conn_max_lifetime", "5m")
+
+	v.SetDefault("services.book.host", "localhost")
+	v.SetDefault("services.book.port", 8081)
 }
